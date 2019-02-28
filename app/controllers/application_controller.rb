ba13bf3a1a2327698b59f39a5a82a8ca8e8b7a03
teppei <suteaka1606135@gmail.com>
2019-02-28 05:37:09 +0000

    init

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,24 @@
+class ApplicationController < ActionController::Base
+  # protect_from_forgery with: :exception
+  
+  # before_action :set_twitter_client
+
+  # def update
+  #   @twitter.update("テスト")
+  #   render plain: "Twitter.update"
+  # end
+
+  # private
+
+  # def set_twitter_client
+  #   @twitter = Twitter::REST::Client.new do |config|
+  #     config.consumer_key        = "YOUR_CONSUMER_KEY"
+  #     config.consumer_secret     = "YOUR_CONSUMER_SECRET"
+  #     config.access_token        = "YOUR_ACCESS_TOKEN"
+  #     config.access_token_secret = "YOUR_ACCESS_SECRET"
+  #   end
+  
+  
+  
+  
+end
