3e25191d91ccc73242a034c1879b94882ce94fdc
teppei <suteaka1606135@gmail.com>
2019-02-28 15:20:13 +0000

    save

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,13 @@ class SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_auth(request.env['omniauth.auth'])
     session[:user_id] = user.uid
-    flash[:notice] = "ユーザ認証が完了しました。"
+    flash[:notice] = "twitter認証が完了しました。"
     redirect_to root_path
   end 
 
   def destroy
-        #     reset_session
-        # flash[:notice] = "ログアウトしました。"
-        # redirect_to root_path
+  session[:user_id] = nil
+  flash[:notice] = "ログアウトしました。"
+  redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,23 @@
 class User < ApplicationRecord
 
-
   def self.find_or_create_from_auth(auth)
-    user.twitter_ID = auth[:info][:nickname]
-    # uid = auth[:uid]
-    user.uid = auth[:uid]
+    uid = auth[:uid]
+
+    self.find_or_create_by(uid: uid) do |user| 
+    #初回のみ
+    user.twitter_ID           = auth[:info][:nickname]
+    user.name                 = auth[:info][:name]
+    user.img_url              = auth[:extra][:raw_info][:profile_image_url].gsub("_normal.","_400x400.")
+    user.description          = auth[:info][:description]
+    user.listed_count         = auth[:extra][:raw_info][:listed_count]
+    user.tweet_count          = auth[:extra][:raw_info][:statuses_count]
+    user.followings_count     = auth[:extra][:raw_info][:friends_count]
+    user.followers_count      = auth[:extra][:raw_info][:followers_count]
+    user.favorites_count      = auth[:extra][:raw_info][:favourites_count]
+    user.is_verified          = auth[:extra][:raw_info][:verified]
+    user.twitter_created_date = auth[:extra][:raw_info][:created_at]
+     
+    end
   end
 
 
