e3b14535d88cff983b442804f4eec59e4e4b2e3f
teppei <suteaka1606135@gmail.com>
2019-03-02 05:20:02 +0000

    css change

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,18 @@ Rails.application.routes.draw do
 
   root to: "toppages#index"
 
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 
 
-  get '/auth/:provider/callback', to: 'sessions#create'
-  get '/auth/twitter/callback', to: 'sessions#create'
+  get "/auth/:provider/callback", to: "sessions#create"
+  get "/auth/twitter/callback", to: "sessions#create"
+
+  get "battles/"
 
 
-  get 'signup', to: 'users#new'
+  get "signup", to: "users#new"
   resources :users, only: [:show, :new, :create]
 
 end
